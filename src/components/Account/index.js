a3f47f0be02f2c174ce8fa8454c9ddb5310d6a83
Marek Urbanek <marek.urbi@gmail.com>
2020-03-09 21:53:02 +0100

    Added authorization HOC

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthUserContext, withAuthorization } from "../Session";
 import { PasswordForgetForm } from "../PasswordForget";
 import PasswordChangeForm from "../PasswordChange";
-const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
-);
-export default AccountPage;
+const AccountPage = () => {
+  const authUser = useContext(AuthUserContext);
+  return (
+    <div>
+      <h1>Account: {authUser.email}</h1>
+      <PasswordForgetForm />
+      <PasswordChangeForm />
+    </div>
+  );
+};
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);
